b939e19000095b55ebe8ff7dabc9915dca0c6740
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-21 11:30:48 -0300

    Adiciona projeção de custo aws rds

--- storage.py
+++ storage.py
@@ -19,9 +19,11 @@ gib_array = bytes_array / (1<<30)
 price_per_gb_aws_dynamo = 0.25
 price_per_gib_cloud_storage = 0.02
 price_per_gib_cloud_sql = 0.17
+price_per_gb_aws_rds = 0.115
 aws_dynamo_price_array = price_per_gb_aws_dynamo * gb_array
 cloud_storage_price_array = price_per_gib_cloud_storage * gib_array
 cloud_sql_price_array = price_per_gib_cloud_sql * gib_array + 49.31
+aws_rds_price_array = price_per_gb_aws_rds * gb_array + 32.12 + 21.90
 
 df = pd.DataFrame({
     'month' : time_array,
@@ -33,6 +35,8 @@ df = pd.DataFrame({
     'cloud storage - cumulative' : np.cumsum(cloud_storage_price_array),
     'cloud sql' : cloud_sql_price_array,
     'cloud sql - cumulative' : np.cumsum(cloud_sql_price_array),
+    'aws rds' : aws_rds_price_array,
+    'aws rds - cumulative' : np.cumsum(aws_rds_price_array)
 })
 df = df.round(2)
 
