c8c502e71917db2b7297db0dc77b0c9e5e9c5446
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-21 11:26:28 -0300

    Adiciona projeção de custo aws dynamodb

--- storage.py
+++ storage.py
@@ -16,11 +16,15 @@ bytes_array = time_array * bytes_per_month
 gb_array = bytes_array / (10 ** 9)
 gib_array = bytes_array / (1<<30)
 
+price_per_gb_aws_dynamo = 0.25
+aws_dynamo_price_array = price_per_gb_aws_dynamo * gb_array
 
 df = pd.DataFrame({
     'month' : time_array,
     'bytes' : bytes_array,
     'gigabytes' : gib_array,
+    'dynamodb' : aws_dynamo_price_array,
+    'dynamodb - cumulative' : np.cumsum(aws_dynamo_price_array),
 })
 df = df.round(2)
 
