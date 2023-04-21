a91ce974a8fba580b4ef848acac7389cc27a8857
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-21 12:00:28 -0300

    Refatora projeções como funções

--- storage.py
+++ storage.py
@@ -16,27 +16,53 @@ bytes_array = time_array * bytes_per_month
 gb_array = bytes_array / (10 ** 9)
 gib_array = bytes_array / (1<<30)
 
-price_per_gb_aws_dynamo = 0.25
-price_per_gib_cloud_storage = 0.02
-price_per_gib_cloud_sql = 0.17
-price_per_gb_aws_rds = 0.115
-aws_dynamo_price_array = price_per_gb_aws_dynamo * gb_array
-cloud_storage_price_array = price_per_gib_cloud_storage * gib_array
-cloud_sql_price_array = price_per_gib_cloud_sql * gib_array + 49.31
-aws_rds_price_array = price_per_gb_aws_rds * gb_array + 32.12 + 21.90
+def aws_dynamo(gb):
+    price_per_gb = 0.25
+    price = gb * price_per_gb
+    price_cum = np.cumsum(price)
+    return {
+        'dynamodb' : price,
+        'dynamodb' : price_cum
+    }
+
+def cloud_storage(gib):
+    price_per_gib = 0.03
+    price = gib * price_per_gib
+    price_cum = np.cumsum(price)
+    return {
+        'cloud storage' : price,
+        'cloud storage - cumulative' : price_cum
+    }
+
+def cloud_sql(gib):
+    price_per_gib = 0.17
+    instance_cost = 49.31
+    price = gib * price_per_gib + instance_cost
+    price_cum = np.cumsum(price)
+    return {
+        'cloud sql' : price,
+        'cloud sql - cumulative' : price_cum
+    }
+
+def aws_rds(gb):
+    price_per_gb = 0.115
+    instance_cost = 32.12
+    proxy_cost = 21.90
+    price = gb * price_per_gb + instance_cost + proxy_cost
+    price_cum = np.cumsum(price)
+    return {
+        'rds' : price,
+        'rds - cumulative' : price_cum
+    }
 
 df = pd.DataFrame({
     'month' : time_array,
     'bytes' : bytes_array,
     'gigabytes' : gib_array,
-    'dynamodb' : aws_dynamo_price_array,
-    'dynamodb - cumulative' : np.cumsum(aws_dynamo_price_array),
-    'cloud storage' : cloud_storage_price_array,
-    'cloud storage - cumulative' : np.cumsum(cloud_storage_price_array),
-    'cloud sql' : cloud_sql_price_array,
-    'cloud sql - cumulative' : np.cumsum(cloud_sql_price_array),
-    'aws rds' : aws_rds_price_array,
-    'aws rds - cumulative' : np.cumsum(aws_rds_price_array)
+    **aws_dynamo(gb_array),
+    **aws_rds(gb_array),
+    **cloud_storage(gib_array),
+    **cloud_sql(gib_array),
 })
 df = df.round(2)
 
