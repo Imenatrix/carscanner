e93c55bbcc877a05db7f91d834bb9be267b944a3
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-27 11:35:45 -0300

    Refatora estimativa de armazenamento sql

--- storage.py
+++ storage.py
@@ -1,34 +1,39 @@
 import numpy as np
 import pandas as pd
 
-bytes_per_row = 2303
 rows_per_minute = 30
 
 rows_per_month = rows_per_minute * 60 * 24 * 30
-bytes_per_month = rows_per_month * bytes_per_row
 
 years = 10
 months = years * 12 + 1
 time = np.arange(months + 1)
 
-bytes_array = time * bytes_per_month
-gb_array = bytes_array / (10 ** 9)
-gib_array = bytes_array / (1<<30)
+def sql(time):
+    bytes_per_row = 330
+    bytes_over_time = rows_per_month * bytes_per_row
+    return bytes_over_time * time
 
-def gb(t):
-    return gb_array
+def gb(bytes_array):
+    return bytes_array / (10 ** 9)
 
-def gib(t):
-    return gib_array
+def gib(bytes_array):
+    return bytes_array / (1<<30)
+
+def sql_gb(time):
+    return gb(sql(time))
+
+def sql_gib(time):
+    return gib(sql(time))
 
 services = {
     'dynamodb' : {
         'cost' : 0.25,
-        'storage' : gb,
+        'storage' : sql_gb,
     },
     'rds' : {
         'cost' : 0.115,
-        'storage' : gb,
+        'storage' : sql_gb,
         'additional_costs' : {
             'server' : 32.12,
             'proxy' : 21.90
@@ -36,18 +41,18 @@ services = {
     },
     'cloud_storage' : {
         'cost' : 0.03,
-        'storage' : gib,
+        'storage' : sql_gb,
     },
     'cloud sql' : {
         'cost' : 0.17,
-        'storage' : gib,
+        'storage' : sql_gb,
         'additional_costs' : {
             'server' : 49.31
         }
     },
     'firestore' : {
         'cost' : 0.18,
-        'storage' : gb,
+        'storage' : sql_gb,
     }
 }
 
